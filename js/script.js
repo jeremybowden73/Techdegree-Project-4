1610ccc43e42c1d1e85225d8f322f1baa8b932b2
jeremybowden73 <jeremybowden73.gmail.com>
2018-06-19 15:15:18 +0100

    Refactored showTilePreview function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /*
-// unadded
+
 		Treehouse techdegree project 4
 
 		TIC TAC TOE
@@ -10,39 +10,48 @@
 */
 
 function game() {
-	function headers(myTurn, notMyTurn) {
-		myTurn.classList.add("active");
-		notMyTurn.classList.remove("active");
+	// function to update the gameboard screen headers depending on whose turn it is (player 1 or player 2)
+	function updateHeaders(currentPlayer) {
+		document.getElementById("player1").classList.remove("active"); // var for the player1 header ('O')
+		document.getElementById("player2").classList.remove("active"); // var for the player2 header ('X')
+		if (currentPlayer === 1) {
+			document.getElementById("player1").classList.add("active");
+		} else {
+			document.getElementById("player2").classList.add("active");
+		}
 	}
 
-	function showPreviewOfOorX(symbol) {
-		console.log(symbol);
-		event.target.style.backgroundImage = "url(img/o.svg)";
-		// if (symbol === "O") {
-		// 	event.target.style.backgroundImage = "url(img/o.svg)";
-		// } else {
-		// 	event.target.style.backgroundImage = "url(img/x.svg)";
-		// }
+	function showTilePreview(player) {
+		if (player === 1) {
+			event.target.style.backgroundImage = "url(img/o.svg)";
+		} else {
+			event.target.style.backgroundImage = "url(img/x.svg)";
+		}
 	}
 
-	// function to invoke when it is player 1 ('O') turn
-	function p1Turn() {
-		player = 1;
-		symbol = "O";
-		headers(player1Header, player2Header);
+
+	// function to invoke when it is the next player's turn
+	function turn(player) {
+		//player = 1;
+		//symbol = "O";
+		updateHeaders(player);
 		const boardUL = document.querySelector(".boxes"); // var for the whole board
-		boardUL.addEventListener("mouseover", showPreviewOfOorX(symbol)); // event listener for when any part of the whole board is moused over
+		boardUL.addEventListener("mouseover", function () {
+			showTilePreview(player)
+		}); // event listener for when any part of the whole board is moused over
+		boardUL.addEventListener("mouseout", function () {
+			event.target.style.backgroundImage = "";
+		}); // event listener for when any part of the whole board is moused out
 	}
 
+	//
+	// start of the game
 	divStart.style.display = "none"; // hide the Start screen
 	//divWinner.style.display = "none"; // hide the Winner screens
 	divBoard.style.display = ""; // unhide the Board screen
 	let player = 1; // let's start with player 1 ('O')
-
-	const player1Header = document.getElementById("player1"); // var for the player1 header ('O')
-	const player2Header = document.getElementById("player2"); // var for the player2 header ('X')
-
-	p1Turn();
+	//let symbol = "O";
+	turn(player);
 
 
 

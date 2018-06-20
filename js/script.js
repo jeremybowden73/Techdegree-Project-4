c933303e294fad46b906d22cd1166242a586e3bf
jeremybowden73 <jeremybowden73.gmail.com>
2018-06-20 13:03:13 +0100

    Updated script.js, gameplay completed

--- js/script.js
+++ js/script.js
@@ -38,7 +38,6 @@ function game() {
 		}
 	}
 
-
 	// function to invoke when it is the next player's turn
 	function turn(player) {
 		if (player === 1) {
@@ -56,10 +55,9 @@ function game() {
 		// event listener for when any part of the whole board is moused over
 		boardUL.addEventListener("mouseover", mousedOver, false);
 		function mousedOver(event) {
-			console.log("mouse in");
-			if (!event.target.classList.contains("box-filled-1") &&
-				!event.target.classList.contains("box-filled-2")) {
-				console.log("you can choose that one");
+			//console.log("mouse in");
+			if (!event.target.classList.contains("box-filled-1") && !event.target.classList.contains("box-filled-2")) {
+				//console.log("you can choose that one");
 				showTilePreview(player);
 			}
 		}
@@ -73,24 +71,61 @@ function game() {
 		// event listener for when a tile is clicked on
 		boardUL.addEventListener("click", clickedOn, false);
 		function clickedOn(event) {
-			if (!event.target.classList.contains("box-filled-1") &&
-				!event.target.classList.contains("box-filled-2")) {
-				console.log("valid selection");
+			if (!event.target.classList.contains("box-filled-1") && !event.target.classList.contains("box-filled-2")) {
+				//console.log("valid selection");
 				boardUL.removeEventListener("mouseover", mousedOver, false);
 				boardUL.removeEventListener("mouseout", mousedOut, false);
 				boardUL.removeEventListener("click", clickedOn, false);
 				selectTile(player);
+				const winner = checkForWinner(player);
+				if (winner) {
+					console.log(`Player ${winner} has won!!!!!!!!!!`);
+					//
+					//
+					// IF winner === 1 or 2, INVOKE THE WINNER SCREEN FOR player
+					// IF winner === 3, INVOKE THE TIED SCREEN
+				}
 				turn(player);
-			} else {
-				console.log("NOT a valid selection");
 			}
 		}
+
+		function checkForWinner(player) {
+			// variable to store the class name that we are going to be searching for
+			let playerClass = "box-filled-1"; // if player === 1
+			if (player === 2) {
+				playerClass = "box-filled-2"; // if player === 2
+			}
+			const allBoardLIs = boardUL.getElementsByTagName("li"); // array of all the tile <li> elements
+			const board = []; // array in which to store true or false values for each tile depending on whether the tile "belongs" to the current player
+			for (let i = 0; i < 9; i++) {
+				if (allBoardLIs[i].classList.contains(playerClass)) {
+					board[i] = true;
+				} else {
+					board[i] = false;
+				}
+			}
+
+			// check for all 8 possible winning combinations and return if the current player is a winner
+			if ((board[0] && board[1] && board[2]) || (board[3] && board[4] && board[5]) ||
+				(board[6] && board[7] && board[8]) || (board[0] && board[3] && board[6]) ||
+				(board[1] && board[4] && board[7]) || (board[2] && board[5] && board[8]) ||
+				(board[0] && board[4] && board[8]) || (board[2] && board[4] && board[6])) {
+				return player;
+			}
+			// check for a tie by counting how many "true" elements are in the board array
+			// if there are 5 then player 1 must have had 5 turns, and there is no winner, so
+			// it must be a tie
+			if (board.filter(item => item === true).length === 5) {
+				return 3; // "player 3" is a tie
+			}
+		}
+
 	}
 
 
 
 	//
-	// start of the game
+	// start the game
 	divStart.style.display = "none"; // hide the Start screen
 	//divWinner.style.display = "none"; // hide the Winner screens
 	divBoard.style.display = ""; // unhide the Board screen

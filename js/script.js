3d4df4087ecc9730dc1d05532e636069797fb6f4
jeremybowden73 <jeremybowden73.gmail.com>
2018-06-21 11:09:08 +0100

    Updated script.js, gameplay updated

--- js/script.js
+++ js/script.js
@@ -9,7 +9,59 @@
 
 */
 
-function game() {
+
+
+
+// initial set-up; create the start screen and the winner screen and hide all the screens
+function setUp() {
+	const divBoard = document.getElementById("board"); // create variable for the Board div (main game board screen)
+	divBoard.style.display = "none"; // hide it initially
+
+	const divBody = divBoard.parentNode;
+	divBody.id = ("body");
+	const divStart = document.createElement("div"); // create element for the Start page div
+	divStart.classList.add("screen", "screen-start"); // give it some classes
+	divStart.id = ("start"); // and give it an id
+	divBody.insertBefore(divStart, divBoard); // insert the Start page div into the <body> element before the Board div
+
+	// inject html content into the Start Screen main div
+	let startScreenHTML =
+		"<header>" +
+		"<h1> Tic Tac Toe</h1>" +
+		"<a href='#' class='button' id='startButton'>Start game</a>" +
+		"</header >";
+	divStart.innerHTML = startScreenHTML;
+
+	const divWinner = document.createElement("div"); // create element for the Winner page div
+	divWinner.classList.add("screen", "screen-win");
+	divWinner.id = ("winner");
+	divBody.insertBefore(divWinner, divBoard);
+
+	let winnerScreenHTML =
+		"<header>" +
+		"<h1>Tic Tac Toe</h1>" +
+		"<p class='message'></p>" +
+		"<a href='#' class='button' id='newGameButton'>New game</a>" +
+		"</header>";
+	divWinner.innerHTML = winnerScreenHTML;
+
+	// divStart.style.display = "none"; // hide the Start screen
+	// divWinner.style.display = "none"; // hide the Winner screens
+	// divBoard.style.display = "none"; // hide the Board screen
+
+	// call the function to show the start screem
+	showStartScreen();
+
+}
+
+function showStartScreen() {
+	hideAllThreeMainDivScreens();
+	document.getElementById("start").style.display = "";
+	const startButton = document.getElementById("startButton");
+	startButton.addEventListener("click", showBoardScreen, false);
+}
+
+function showBoardScreen() {
 	// function to update the gameboard screen headers depending on whose turn it is (player 1 or player 2)
 	function updateHeaders(player) {
 		if (player === 1) {
@@ -32,7 +84,7 @@ function game() {
 	function selectTile(player) {
 		if (player === 1) {
 			event.target.classList.add("box-filled-1"); // changes the display of the tile per player1 colour
-		} else if (player === 2) {
+		} else {
 			event.target.classList.add("box-filled-2"); // changes the display of the tile per player2 colour
 		}
 	}
@@ -79,13 +131,10 @@ function game() {
 				selectTile(player);
 				const winner = checkForWinner(player, boardUL);
 				if (winner) {
-					console.log(`Player ${winner} has won!!!!!!!!!!`);
-					//
-					//
-					// IF winner === 1 or 2, INVOKE THE WINNER SCREEN FOR player
-					// IF winner === 3, INVOKE THE TIED SCREEN
+					showWinnerScreen(winner);
+				} else {
+					turn(player);
 				}
-				turn(player);
 			}
 		}
 	}
@@ -111,79 +160,54 @@ function game() {
 			(board[6] && board[7] && board[8]) || (board[0] && board[3] && board[6]) ||
 			(board[1] && board[4] && board[7]) || (board[2] && board[5] && board[8]) ||
 			(board[0] && board[4] && board[8]) || (board[2] && board[4] && board[6])) {
+			resetBoardElements();
 			return player;
 		}
-		// check for a tie by counting how many "true" elements are in the board array
-		// if there are 5 then player 1 must have had 5 turns, and there is no winner, so
-		// it must be a tie
+		// check for a tie by counting how many "true" elements are in the board array if there
+		// are 5 then player 1 must have had 5 turns, and there is no winner, so it must be a tie
 		if (board.filter(item => item === true).length === 5) {
+			resetBoardElements();
 			return 3; // "player 3" is a tie
+		}
+
+		// function to reset the board tiles if we had a winner or a tie
+		function resetBoardElements() {
+			for (let j = 0; j < 9; j++) {
+				allBoardLIs[j].classList.remove("box-filled-1");
+				allBoardLIs[j].classList.remove("box-filled-2");
+			}
 		}
 	}
 
-
-
-
-	//
-	// start the game by hiding and showing the required screen divs
-	document.getElementById("start").style.display = "none";
+	// to start the game, hide all screen divs except "board"
+	hideAllThreeMainDivScreens();
 	document.getElementById("board").style.display = "";
+	turn(2); // invoke the "turn" function for the first move to take place (turn toggles the player, so passing 2 to it will result in player 1 having the first go)
+
+}
+
+function showWinnerScreen(player) {
+	hideAllThreeMainDivScreens();
+	const winnerScreen = document.getElementById("winner");
+	winnerScreen.style.display = "";
+	if (player === 1) {
+		winnerScreen.classList.add("screen-win-one");
+	} else if (player === 2) {
+		winnerScreen.classList.add("screen-win-two");
+	} else {
+		winnerScreen.classList.add("screen-win-tie");
+	}
+
+	const newGameButton = document.getElementById("newGameButton");
+	newGameButton.addEventListener("click", showBoardScreen, false);
+}
+
+function hideAllThreeMainDivScreens() {
+	document.getElementById("start").style.display = "none";
+	document.getElementById("board").style.display = "none";
 	document.getElementById("winner").style.display = "none";
-	let player = 2; // player toggles when the next (in this case first) turn takes place, so setting player to 2 initially means player 1 will actually have the first turn
-	console.log("starting with player 2");
-	turn(player); // invoke the function "turn" for the first move to take place
-
 }
 
-
-// initial set-up; hide gameboard and show the start screen
-
-function setUp() {
-	const divBoard = document.getElementById("board"); // create variable for the Board div (main game board screen)
-	divBoard.style.display = "none"; // hide it initially
-
-	const divBody = divBoard.parentNode;
-	divBody.id = ("body");
-	const divStart = document.createElement("div"); // create element for the Start page div
-	divStart.classList.add("screen", "screen-start"); // give it some classes
-	divStart.id = ("start"); // and give it an id
-	divBody.insertBefore(divStart, divBoard); // insert the Start page div into the <body> element before the Board div
-
-	// inject html content into the Start Screen main div
-	let startScreenHTML =
-		"<header>" +
-		"<h1> Tic Tac Toe</h1>" +
-		"<a href='#' class='button' id='startButton'>Start game</a>" +
-		"</header >";
-	divStart.innerHTML = startScreenHTML;
-
-	const divWinner = document.createElement("div"); // create element for the Winner page div
-	divWinner.classList.add("screen", "screen-win");
-	divWinner.id = ("winner");
-	divBody.insertBefore(divWinner, divBoard);
-
-	let winnerScreenHTML =
-		"<header>" +
-		"<h1>Tic Tac Toe</h1>" +
-		"<p class='message'></p>" +
-		"<a href='#' class='button' id='newGameButton'>New game</a>" +
-		"</header>";
-	divWinner.innerHTML = winnerScreenHTML;
-
-	divStart.style.display = "none"; // hide the Start screen
-	divWinner.style.display = "none"; // hide the Winner screens
-	divBoard.style.display = "none"; // hide the Board screen
-
-	showStartScreen();
-
-}
-
-function showStartScreen() {
-	document.getElementById("start").style.display = "";
-
-	const startButton = document.getElementById("startButton");
-	startButton.addEventListener("click", game, false);
-}
 
 setUp();
 
